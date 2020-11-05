90697f08410255faba10e5ec2cf0a07feb3e631f
Christian Shaw <cshaw00048@gmail.com>
2020-11-04 21:33:52 -0600

    adding js keyboard

--- assets/js/wordguess.js
+++ assets/js/wordguess.js
@@ -37,4 +37,16 @@ function randomWord() {
     answer = disney[Math.floor(Math.random() * disney.length)];
 }
 
-randomWord();
+function generateButtons() {
+    let buttonsHTML = "abcdefghijklmnopqrstuvwxyz".split("").map(letter=>
+    `
+    <button 
+        class="btn btn-lg btn-primary m-2"
+        id='`+ letter + `'
+        onClick="handleGuess('` + letter + `')"
+    >
+
+    ` + letter + `
+    </button>
+    `).join("");
+}
